4f1eb5dbe8b5050d9264d52ce0a58529740e1383
Gavin <G00402296@atu.ie>
2023-11-07 15:02:53 +0000

    First Commit

---
+++ src/main/java/ie/atu/labexamgw/LabExamGwApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamGwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/labexamgw/LabExamGwApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.labexamgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabExamGwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
